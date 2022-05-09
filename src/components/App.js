943e53d161f82d70a73160686e27da5d4a7858ab
Natalia Teixeira <nataliat1026@gmail.com>
2022-05-09 18:36:35 -0400

    browser router initiated in index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import NavBar from './NavBar';
 import Upgrades from './Upgrades';
@@ -8,10 +9,11 @@ function App() {
   return (
     <>
       <NavBar/>
-      <p>TEST</p>
+
       <Home/>
       <Upgrades/>
       <Profile/>
+      
     </>
   );
 }
