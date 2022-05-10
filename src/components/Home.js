aab7f752c36d6589190596fb50b8e1b4297bfaeb
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-05-09 20:03:20 -0400

    Button generates a cat

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CatList from './CatList'
 import TrapCat from './TrapCat'
 
 function Home() {
+  const [cats, setCats] = useState([])
+
+  useEffect(()=>{fetch('http://localhost:3001/cats')
+  .then(r=>r.json())
+  .then(data=>setCats(data))}
+  ,[])
+
+  // console.log(cats)
+
   return (
     <div>Home
         <TrapCat/>
--- src/components/TrapCat.js
+++ src/components/TrapCat.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function TrapCat() {
+  const [newCat, setNewCat] = useState({
+    name:"",
+    image:""
+  })
+
+  useEffect(()=>updateCat,[])
+
+  function updateCat(){
+    let url=""
+    fetch("https://api.thecatapi.com/v1/images/search")
+    .then(r=>r.json())
+    .then(data=>url=data[0]['url'])
+
+    fetch("https://randomuser.me/api/")
+    .then(r=>r.json())
+    .then(data=>{
+      data['results'][0]['name']['first']!==data['results'][0]['name']['first'].toLowerCase()?setNewCat({name:data['results'][0]['name']['first'], image:url}):updateCat()})
+    }
+
+  console.log(newCat)
+  function generateRandomCat(){
+    fetch("http://localhost:3001/cats",{
+      method:"POST",
+      headers:{
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newCat)
+    })
+    .then(r=>r.json())
+    .then(data=>console.log(data))
+    updateCat()
+  }
   return (
-    <div>TrapCat</div>
+    <div>TrapCat
+      <button onClick={
+        generateRandomCat
+      }>Catch A Cat</button>
+    </div>
   )
 }
 
